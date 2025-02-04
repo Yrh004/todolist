f7e54b956f5ffa5500b872af438c30413f1072c0
yrh004 <yhines004@gmai.com>
2025-02-04 00:23:34 -0600

    dfgf

--- ToDo/feathers-backend/my-feathers-app/src/services/index.ts
+++
@@ -1,6 +0,0 @@
-import { Application } from '../declarations';
-import { todo } from './todo/todo.service';
-
-export const services = (app: Application) => {
-  app.configure(todo);
-};
--- ToDo/my-todo-app/src/App.tsx
+++ ToDo/my-todo-app/src/App.tsx
@@ -1,11 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TodoList from "./components/TodoList";
 import TodoInput from "./components/TodoInput";
 import { Todo } from "./types";
+import { todoService } from "./feathers";
+import reactlogo from './assets/react.svg';
+import vitelogo from '../public/vite.svg';
 import './App.css';
 
 const App: React.FC = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
+
+  useEffect(() => {
+    // Fetch initial todos
+    todoService.find().then((res: any) => setTodos(res.data));
+
+    // Listen for real-time updates
+    todoService.on("created", (todo: Todo) => setTodos((prev) => [...prev, todo]));
+    todoService.on("patched", (updatedTodo: Todo) =>
+      setTodos((prev) => prev.map((todo) => (todo.id === updatedTodo.id ? updatedTodo : todo)))
+    );
+    todoService.on("removed", (deletedTodo: Todo) =>
+      setTodos((prev) => prev.filter((todo) => todo.id !== deletedTodo.id))
+    );
+  }, []);
 
   const addTodo = (text: string) => {
     setTodos([...todos, { id: Date.now(), text, completed: false }]);
@@ -21,6 +38,10 @@ const App: React.FC = () => {
 
   return (
     <div>
+      <a >
+        <img src={vitelogo} className="logo" />
+        <img src={reactlogo} className="logo"/>
+      </a>
       <h1>Todo List</h1>
       <TodoInput addTodo={addTodo} />
       <TodoList todos={todos} toggleTodo={toggleTodo} removeTodo={removeTodo} />
